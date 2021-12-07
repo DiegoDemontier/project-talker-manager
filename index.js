2a87c178763e508d31e84cc6bc1930094e269994
Demontier <demontier@outlook.com>
2021-12-07 19:22:58 -0300

    requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,18 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (req, res) => {
+  try {
+    const data = fs.readFileSync('talker.json', 'utf8');
+    if (JSON.parse(data).length === 0) {
+      return res.status(HTTP_OK_STATUS).json('[]');
+    } 
+      return res.status(HTTP_OK_STATUS).json(JSON.parse(data));
+  } catch (err) {
+    console.log(err.message);
+  }
 });
 
 app.listen(PORT, () => {
